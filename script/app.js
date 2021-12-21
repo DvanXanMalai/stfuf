d188ffe2898eb502a24c1df9cef7d2960748f778
DvanXanMalai <xabilal27@gmail.com>
2021-12-21 17:50:21 +0545

    stfufer

---
+++ script/app.js
@@ -0,0 +1,10 @@
+const billInput=document.querySelector('#bill').value
+const peopleInput=document.querySelector('#no-of-people').value
+const tipPerPerson=document.getElementById('tip-amount')
+const totalPerPerson=document.getElementById('total-amount')
+
+
+billInput.value='0.0'
+peopleInput.value='1'
+
+alert(billInput)
